a2d24ef9cda9f88fc2f0fc8be5384857b814bdaa
akkuplus <github@akkuplus.33mail.com>
2021-06-08 13:16:19 +0200

    implemented temporary Connection to remote PostgreSQL DBS

--- create_tables.py
+++ create_tables.py
@@ -2,26 +2,35 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
-def create_database():
+def create_database(host="127.0.0.1", dbname="studentdb", user="student", password="student"):
     """
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    #conn = psycopg2.connect(host=host dbname=dbname user=user password=password)
+    conn_details = {"host":"tai.db.elephantsql.com",
+                    "dbname":"xnzxiluz",
+                    "user":"xnzxiluz",
+                    "password":"Ygv_W6DvmeC6Jtbo1VxhrQ4Yjgk2z2fd"
+    }
+    conn = psycopg2.connect(**conn_details)
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    #cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    #cur.execute("DROP DATABASE IF EXISTS xnzxiluz")
+    #cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    #cur.execute("CREATE DATABASE xnzxiluz WITH ENCODING 'utf8' TEMPLATE template0")
 
     # close connection to default database
     conn.close()    
     
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    #conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(**conn_details)
     cur = conn.cursor()
     
     return cur, conn
