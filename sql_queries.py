7b2b987d038d13d15ae36d77cb7db1954473e0bd
akkuplus <github@akkuplus.33mail.com>
2021-06-08 14:52:27 +0200

    implemented draft CREATE and DROP statement for tables
    REMAINING: check table schema!
    Signed-off-by: akkuplus <github@akkuplus.33mail.com>

--- sql_queries.py
+++ sql_queries.py
@@ -8,20 +8,41 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("""CREATE TABLE songplays(songplay_id int, 
+                                                    start_time int, 
+                                                    user_id int, 
+                                                    level varchar, 
+                                                    song_id varchar, 
+                                                    artist_id varchar, 
+                                                    session_id varchar, 
+                                                    location varchar, 
+                                                    user_agent varchar)""")
 
-user_table_create = ("""
-""")
+user_table_create = ("""CREATE TABLE users(user_id int, 
+                                            first_name varchar, 
+                                            last_name varchar,
+                                            gender varchar, 
+                                            level varchar)""")
 
-song_table_create = ("""
-""")
+song_table_create = ("""CREATE TABLE songs(song_id varchar, 
+                                            title varchar, 
+                                            artist_id varchar, 
+                                            year int, 
+                                            duration float)""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE artists(artist_id varchar, 
+                                                name varchar, 
+                                                location varchar, 
+                                                latitude varchar, 
+                                                longitude varchar)""")
 
-time_table_create = ("""
-""")
+time_table_create = ("""CREATE TABLE time(start_time int, 
+                                            hour int, 
+                                            day int, 
+                                            week int, 
+                                            month int, 
+                                            year int, 
+                                            weekday int)""")
 
 # INSERT RECORDS
 
