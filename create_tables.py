a2922cf135334d849ae4721101c0dcff2bd860cd
akkuplus <github@akkuplus.33mail.com>
2021-06-10 00:10:39 +0200

    reworked procedure to drop and (re-)create a database -> set behaviour for commits
    Signed-off-by: akkuplus <github@akkuplus.33mail.com>

--- create_tables.py
+++ create_tables.py
@@ -1,37 +1,47 @@
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 from sql_queries import create_table_queries, drop_table_queries
 
 
-def create_database(host="127.0.0.1", dbname="studentdb", user="student", password="student"):
+def create_database(host="127.0.0.1", dbname="postgres", user="student", password="student", external_postgres_dbs=False):
     """
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
     
     # connect to default database
-    #conn = psycopg2.connect(host=host dbname=dbname user=user password=password)
-    conn_details = {"host":"tai.db.elephantsql.com",
-                    "dbname":"xnzxiluz",
-                    "user":"xnzxiluz",
-                    "password":"Ygv_W6DvmeC6Jtbo1VxhrQ4Yjgk2z2fd"
-    }
-    conn = psycopg2.connect(**conn_details)
-    conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
-    # create sparkify database with UTF8 encoding
-    #cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    #cur.execute("DROP DATABASE IF EXISTS xnzxiluz")
-    #cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
-    #cur.execute("CREATE DATABASE xnzxiluz WITH ENCODING 'utf8' TEMPLATE template0")
+    if external_postgres_dbs:
+        conn_details = {"host":"tai.db.elephantsql.com",
+                        "dbname":"xnzxiluz",
+                        "user":"xnzxiluz",
+                        "password":"Ygv_W6DvmeC6Jtbo1VxhrQ4Yjgk2z2fd"
+        }
+        conn = psycopg2.connect(**conn_details)
+        conn.set_session(autocommit=True)
+        cur = conn.cursor()
 
-    # close connection to default database
-    conn.close()    
-    
-    # connect to sparkify database
-    #conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    conn = psycopg2.connect(**conn_details)
-    cur = conn.cursor()
+    else:
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+
+        # create sparkify database with UTF8 encoding
+        # Before, give user student permission:
+            # postgres =  # "ALTER USER student CREATEDB CREATEROLE LOGIN;"
+        conn.commit()
+        cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+
+        cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+        conn.commit()
+
+        # close connection to default database
+        conn.close()
+
+        # connect to sparkify database
+        conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+        conn.set_session(autocommit=True)
+        cur = conn.cursor()
     
     return cur, conn
 
